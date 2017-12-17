32aee42626ce4374d8b9e7ed10cce02fa35ed5a0
Philipp Muens <raute1337@gmx.de>
2017-12-17 12:51:04 +0100

    Add isDockerRunning util

---
+++ src/util/isDockerRunning.js
@@ -0,0 +1,9 @@
+import execa from 'execa'
+
+async function isDockerRunning() {
+  return execa('docker', [ 'version' ])
+    .then(() => true)
+    .catch(() => false)
+}
+
+export default isDockerRunning
---
+++ src/util/isDockerRunning.test.js
@@ -0,0 +1,28 @@
+import execa from 'execa'
+import Promise from 'bluebird'
+import isDockerRunning from './isDockerRunning'
+
+jest.mock('execa')
+
+execa.mockImplementationOnce(() => Promise.resolve())
+execa.mockImplementationOnce(() => Promise.reject())
+
+afterAll(() => {
+  execa.mockRestore()
+})
+
+describe('isDockerRunning()', () => {
+  it('should detect if Docker is running', async () => {
+    const res = await isDockerRunning()
+
+    expect(execa).toHaveBeenCalledWith('docker', [ 'version' ])
+    expect(res).toEqual(true)
+  })
+
+  it('should return false if Docker is not running', async () => {
+    const res = await isDockerRunning()
+
+    expect(execa).toHaveBeenCalledWith('docker', [ 'version' ])
+    expect(res).toEqual(false)
+  })
+})
