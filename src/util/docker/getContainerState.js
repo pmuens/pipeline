7d8571b214d4dd42d40908ca492e4a919e9b8091
Philipp Muens <raute1337@gmx.de>
2017-12-20 20:59:21 +0100

    Remove getContainerState util

--- src/util/docker/getContainerState.js
+++
@@ -1,8 +0,0 @@
-import execa from 'execa'
-
-async function getContainerState(containerId) {
-  return execa('docker', [ 'inspect', containerId, '--format', '{{json .State}}' ]).then((res) =>
-    JSON.parse(res.stdout))
-}
-
-export default getContainerState
