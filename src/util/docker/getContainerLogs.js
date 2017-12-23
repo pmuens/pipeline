4faab3f87e67b76da8e76df8d3723ebd63afe53b
Philipp Muens <raute1337@gmx.de>
2017-12-23 22:53:08 +0100

    Fix getContainerLogs to return an array of stdout and stderr log objects

--- src/util/docker/getContainerLogs.js
+++ src/util/docker/getContainerLogs.js
@@ -1,7 +1,24 @@
 import execa from 'execa'
+import { union, ascend, prop, sortWith, map } from 'ramda'
 
+// TODO refactor this code to use less in-memory operations
 async function getContainerLogs(containerId) {
-  return execa('docker', [ 'logs', containerId ]).then((res) => res.stdout)
+  return execa('docker', [ 'logs', '--timestamps', containerId ]).then((res) => {
+    const { stderr, stdout } = res
+
+    const output = union(stdout.split('\n'), stderr.split('\n'))
+
+    const logs = map((log) => {
+      const matchRes = log.match(/(.+Z )(.*)/)
+      return {
+        timestamp: matchRes[1].trim(), // remove space between timestamp and message
+        message: matchRes[2]
+      }
+    }, output)
+
+    const sorter = sortWith([ ascend(prop('timestamp')) ])
+    return sorter(logs)
+  })
 }
 
 export default getContainerLogs
--- src/util/docker/getContainerLogs.test.js
+++ src/util/docker/getContainerLogs.test.js
@@ -3,19 +3,30 @@ import getContainerLogs from './getContainerLogs'
 
 jest.mock('execa')
 
-execa.mockImplementation(() => Promise.resolve({ stdout: 'some logs' }))
+execa.mockImplementation(() =>
+  Promise.resolve({
+    stdout:
+      '1970-01-03T20:12:15.790078302Z some stdout logs 1\n1970-01-01T20:12:15.790078302Z some stdout logs 2',
+    stderr:
+      '1970-01-02T20:12:15.790078302Z some stderr logs 1\n1970-01-04T20:12:15.790078302Z some stderr logs 2'
+  }))
 
 afterAll(() => {
   execa.mockRestore()
 })
 
 describe('getContainerLogs()', () => {
-  it('should return the container logs', async () => {
+  it('should return a sorted array with log objects', async () => {
     const containerId = 'some-container-id'
 
     const logs = await getContainerLogs(containerId)
 
-    expect(logs).toEqual('some logs')
-    expect(execa).toHaveBeenCalledWith('docker', [ 'logs', containerId ])
+    expect(logs).toEqual([
+      { timestamp: '1970-01-01T20:12:15.790078302Z', message: 'some stdout logs 2' },
+      { timestamp: '1970-01-02T20:12:15.790078302Z', message: 'some stderr logs 1' },
+      { timestamp: '1970-01-03T20:12:15.790078302Z', message: 'some stdout logs 1' },
+      { timestamp: '1970-01-04T20:12:15.790078302Z', message: 'some stderr logs 2' }
+    ])
+    expect(execa).toHaveBeenCalledWith('docker', [ 'logs', '--timestamps', containerId ])
   })
 })
